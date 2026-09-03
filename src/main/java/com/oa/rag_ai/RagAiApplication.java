0f29844d9a15e2ebd5a5511f851352c919d454d4
小灰灰 <1373301763@qq.com>
2026-09-03 18:53:27 +0800

    chore: 初始化 Spring Boot RAG AI 项目

---
+++ src/main/java/com/oa/rag_ai/RagAiApplication.java
@@ -0,0 +1,13 @@
+package com.oa.rag_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oa/rag_ai/RagAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oa.rag_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
